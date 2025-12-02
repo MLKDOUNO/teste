5452d2d7021590a5b05ea1dc9e4e4631cd7a7b6f
MLKDOUNO <edivisonbr22@gmail.com>
2025-12-02 17:35:09 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,4 @@
+function sd(){
+  let elemento = document.getElementById("btn");
+  elemento.innerHTML = "<img id='b' src='/img/b.jpg' ><img id='c'src='/img/c.jpg'><img id='d'src='/img/d.jpg'><img id='e'src='/img/e.jpg'><img id='f'src='/img/f.jpg'><img id='g'src='/img/g.jpg'><img id='h'src='/img/h.jpg'> ";
+};
