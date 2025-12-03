11e18078faa9ccc266d3d8541849b8c824a16c37
MLKDOUNO <edivisonbr22@gmail.com>
2025-12-03 17:46:36 -0300

    Atualizar o js.js

--- js/js.js
+++ js/js.js
@@ -1,4 +1,4 @@
 function sd(){
   let elemento = document.getElementById("btn");
-  elemento.innerHTML = "<img id='b' src='img/b.jpg' ><img id='c'src='img/c.jpg'><img id='d'src='img/d.jpg'><img id='e'src='img/e.jpg'><img id='f'src='img/f.jpg'><img id='g'src='img/g.jpg'><img id='h'src='img/h.jpg'> <img id='i'src='img/i.jpg'><img id='j'src='img/j.jpg'><img id='k'src='img/k.jpg'><img id='l'src='img/l.jpg'><a> 1 MES DE NÓS <br>EU TE AMO <br> MIL MILHOES❤️</a>";
+  elemento.innerHTML = "<img id='b' src='img/c.jpg' ><img id='c'src='img/c.jpg'><img id='d'src='img/d.jpg'><img id='e'src='img/e.jpg'><img id='f'src='img/f.jpg'><img id='g'src='img/g.jpg'><img id='h'src='img/h.jpg'> <img id='i'src='img/i.jpg'><img id='j'src='img/j.jpg'><img id='k'src='img/k.jpg'><img id='l'src='img/l.jpg'><a> 1 MES DE NÓS <br>EU TE AMO <br> MIL MILHOES❤️</a>";
 };
